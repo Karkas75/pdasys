c60624c487ad0bd2b8187dd1886bbd52c8e8fb05
PDA System <pda@restaurant.gr>
2026-04-02 02:33:09 +0300

    Fix Express route - use app.use() instead of app.get('*')

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 });
 
 // Fallback for any route - serve index.html (for PWA)
-app.get('*', (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
